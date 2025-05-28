5caafc6ef1c4c9461f91c0f09913c8e4a0c5a8f4
Thainá Foltran <thainafoltran@hotmail.com>
2025-05-28 11:01:53 -0300

    criei pasta database e ajustei model

--- model_reserva.py
+++ model_reserva.py
@@ -1,6 +1,9 @@
 from database import db
 
 class Reserva(db.Model):
+    __tablename__ = 'reserva'
+
+    
     id_sala = db.Column(db.Integer, primary_key=True, nullable=False)
     periodo = db.Column(db.String (100), nullable=False)
     data = db.Column (db.Date, nullable=False)
